95f58454c610d216a47088dfc008bf06f3b4ac35
temic137 <68796483+temic137@users.noreply.github.com>
2024-11-06 00:09:01 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    githubToken: '',
+    githubToken: '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',
     githubApiUrl: 'https://api.github.com',
   };
   
